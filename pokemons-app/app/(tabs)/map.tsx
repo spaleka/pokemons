c613f82a5fed86c6f7db73a3b388d323d64e58dc
spaleka <agata_spalek@interia.eu>
2025-07-10 08:43:45 +0200

    map fix

--- pokemons-app/app/(tabs)/map.tsx
+++ pokemons-app/app/(tabs)/map.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 //TODO bottom sheet
 
 export default function Map() {
+  const latitude = 37.78825; // Example latitude
+  const longitude = -122.4324; // Example longitude
   return (
     <View style={{ flex: 1 }}>
-      <MapView style={StyleSheet.absoluteFill} provider={PROVIDER_GOOGLE} />
+      <MapView
+        style={StyleSheet.absoluteFill}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: latitude, longitude: longitude }}
+          // image={{ uri: "custom_pin" }}
+        />
+      </MapView>
       {/* <Text>Map is under constructiion</Text> */}
     </View>
   );
